4512718ffa6fef914be5f5a017b23bc60ab9ffc2
sunmengxin <mentionlyze@outlook.com>
2023-12-04 11:49:32 +0800

    fix

--- opencv_config/main.cpp
+++ opencv_config/main.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
-#include "fmt/core.h"
-#include <opencv2/opencv.hpp>
+#include "opencv2/opencv.hpp"
 
 using namespace cv;
 
-int main() {
-  Mat src = imread("C:/Users/menti/Pictures/pexels-photo-2174974.webp");
-  if (src.empty()) {
+int main()
+{
+  Mat src = imread("C:/Users/menti/Pictures/9946780-8e3a6d3a9c4289c3.jpg");
+  if (src.empty())
+  {
     printf("No Image");
     return -1;
-  } 
-    namedWindow("Input", WINDOW_AUTOSIZE);
-    imshow("Input", src);
-    waitKey(0);
-    destroyAllWindows();
-    return 0;
+  }
+  namedWindow("Input", WINDOW_AUTOSIZE);
+  imshow("Input", src);
+  waitKey(0);
+  destroyAllWindows();
+  return 0;
 }
